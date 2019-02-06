05028da314067e0b8241fa72f21bc770efb043e8
visualstudio <visualstudio@122-PC01>
2019-02-06 10:04:51 -0500

    10.00

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -6,7 +6,27 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MethodA();
+            Console.WriteLine();
         }
+        public void MethodA()
+        { int i;
+            while (Peanut()){ 
+                    Console.WriteLine("i is{0},"i);
+                    if(i>10)
+                {
+                    return;
+                }
+                }
+
+            }
+        }
+            
+        public static bool Peanut()
+        {
+            i++;
+            return true;
+        }
+       
     }
-}
+
